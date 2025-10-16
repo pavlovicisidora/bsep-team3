54343589195cc0f9d5d971b96a96e2da0fe871c5
Srdjan Vukojevic <srdjan.vukojevic002@gmail.com>
2025-10-16 22:55:56 +0200

    Implemented EE sertificate

---
+++ pki-service/src/main/java/rs/ac/uns/ftn/bsep/pki_service/dto/CreateEeCertificateDto.java
@@ -0,0 +1,10 @@
+package rs.ac.uns.ftn.bsep.pki_service.dto;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class CreateEeCertificateDto {
+    private String issuerSerialNumber;
+    private Date validTo;
+}
